2e73eebea66b2a01e84995cfb09ee15d26c80913
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-28 13:33:13 +0500

    todolist completed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import './App.css';
 import 'react-notifications/lib/notifications.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import { NotificationContainer, NotificationManager } from 'react-notifications';
 
-
 function App() {
+  const [todolist, setTodolist] = useState([]);
 
-  let [todolist, setTodolist] = useState([])
-
-  let saveToDoList = (event) => {
-
-    let todoName = event.target.todoName.value;
-
-    if (!todolist.includes(todoName)) {
-      let finalToDoList = [...todolist, todoName]
-      setTodolist(finalToDoList)
-    } else {
-      // NotificationManager.warning('This task is already exist.', 'Warning');
-      NotificationManager.error('This task is already exist.', 'Error', 5000,);
-    }
-
+  const saveToDoList = (event) => {
     event.preventDefault();
+    const todoName = event.target.todoName.value;
 
-  }
+    if (!todolist.find(item => item.name === todoName)) {
+      const finalToDoList = [...todolist, { name: todoName, completed: false }];
+      setTodolist(finalToDoList);
+      event.target.todoName.value = '';
+      NotificationManager.success('Your task is successfully added.', 'Done');
+    } else {
+      NotificationManager.error('This task is already exist.', 'Error', 5000);
+    }
+  };
+
+  const toggleCompletion = (index) => {
+    const updatedTodolist = [...todolist];
+    updatedTodolist[index].completed = !updatedTodolist[index].completed;
+    setTodolist(updatedTodolist);
+  };
+
+  const deleteTask = (index) => {
+    const dConfirm = window.confirm("Do you want to delete this task?");
+    if (dConfirm) {
+      const finalData = todolist.filter((_, i) => i !== index);
+      setTodolist(finalData);
+      NotificationManager.info('Your task successfully deleted!', 'Deleted');
+
+    }
+  };
+
   return (
-    <div className="App w-full flex items-center flex-col justify-center">
+    <div className="App w-full flex items-center flex-col justify-start">
       <NotificationContainer />
       <h1 className='select-none w-full text-center text-5xl m-10 font-[500]'>ToDo List</h1>
       <form onSubmit={saveToDoList}>
         <input type="text" name='todoName' />
         <button><FontAwesomeIcon icon={faPlus} /></button>
-        <button className='bg-red-600'><FontAwesomeIcon icon={faTrash} /></button>
       </form>
+      <ul>
+        {todolist.map((item, index) => (
+          <li key={index}>
+            <p className={item.completed ? "line-through text-zinc-500" : "no-underline"}>{item.name}</p>
+            <span>
+              <button onClick={() => toggleCompletion(index)} className='delete-button bg-[#008000]'><FontAwesomeIcon icon={faCheck} /></button>
+              <button onClick={() => deleteTask(index)} className='bg-red-600 delete-button'><FontAwesomeIcon icon={faTrash} /></button>
+            </span>
+          </li>
+        ))}
+      </ul>
     </div >
   );
 }
